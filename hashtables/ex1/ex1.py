26ecffb3e536c6bd93a81db1f52eabde946e23c9
Marco Whyte <mwhyte@umass.edu>
2020-07-10 11:29:19 -0500

    mvp and stretch

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,18 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
-    return None
+    cache = []
+    res = []
+    if length < 2:
+        return None
+
+    for i in range(length):
+        diff = limit - weights[i]
+        print(diff)
+        if diff in cache:
+            res.append(i)
+            res.append(cache.index(diff))
+            return res
+        else:
+            cache.append(weights[i])
+    # print(cache)
